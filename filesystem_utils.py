ecdcb25e7591c052dadd0587117f87a19bfc1461
bkutasi <balazs@kutasi.dev>
2023-10-05 23:18:04 +0200

    Filesystem class initial

--- filesystem_utils.py
+++ filesystem_utils.py
@@ -1,62 +1,68 @@
 import os
 import mne
 from pathlib import Path
+import logging
+from typing import List, Union
+
+# TODO validate + write a test for each function
+
+# Create a custom logger
+logger = logging.getLogger(__name__)
 
 
 class FilesystemUtils:
-    @staticmethod
-    def list_files(self):
-        # This only lists files in cwd
-        # prints the files found
-        # maybe use for sanity check later
-        path_of_the_directory = os.getcwd()
-        # path_of_the_directory= 'EEG_Physionet'
-        print("Files and directories in a specified path:")
+    def __init__(self, debug: bool = False):
+        self.debug = debug
+
+    def list_files(self) -> None:
+        """
+        Lists files in the current working directory.
+        """
+        path_of_the_directory: str = os.getcwd()
+        logger.info("Files and directories in a specified path:")
         for filename in os.listdir(path_of_the_directory):
-            f = os.path.join(path_of_the_directory, filename)
+            f: str = os.path.join(path_of_the_directory, filename)
             if os.path.isfile(f):
-                print(f)
+                logger.info(f)
 
-    @staticmethod
-    def read_dir_files(self):
-        # this reads in folders and files
-        # python 3.5 style with scandir
-        # the files & dirs in the root(current working dir) are saved in arrays
-        # prints the dirs found, but the object are not simple strings
-        # I can't make use of it currently
-
-        folders = []
-        folder_paths = []
-        files = []
+    def read_dir_files(self) -> None:
+        """
+        Reads in folders and files in the current working directory.
+        """
+        folders: List[str] = []
+        folder_paths: List[str] = []
+        files: List[str] = []
         for entry in os.scandir(os.getcwd()):
             if entry.is_dir():
                 folders.append(entry)
                 folder_paths.append(entry.path)
             elif entry.is_file():
                 files.append(entry.path)
-        print("Folders:")
+        logger.info("Folders:")
         for f in folders:
-            print(f)
+            logger.info(f)
 
-    @staticmethod
-    def dir_list(self):
-        # this takes the list of dirs and put them into a list
-        dirlist = [
+    def dir_list(self) -> List[str]:
+        """
+        Returns a list of directories in the current working directory.
+        """
+        dirlist: List[str] = [
             item
             for item in os.listdir(os.getcwd())
             if os.path.isdir(os.path.join(os.getcwd(), item))
         ]
 
         if self.debug:
-            print(dirlist)
+            logger.info(dirlist)
 
         return dirlist
 
-    @staticmethod
-    def open_subj_files(self, subj_single):
-        # This lists the files in certain subject dir, filters *.edf files for safety
-        source_dir = Path(subj_single)
-        filelist = os.listdir(source_dir)
+    def open_subj_files(self, subj_single: str) -> int:
+        """
+        Lists the files in a certain subject directory, filters *.edf files for safety.
+        """
+        source_dir: Union[Path, str] = Path(subj_single)
+        filelist: List[str] = os.listdir(source_dir)
 
         for f in filelist:
             raw = mne.io.read_raw_edf(f)
@@ -66,17 +72,7 @@ class FilesystemUtils:
                 raw, events, event_id=event_dict, tmin=0, tmax=4, baseline=None
             )
 
-        # below is file processing if needed in any case
-        """
-        files = source_dir.glob('*.edf')
-        for file in files:
-            with file.open('r') as file_handle:
-                for single_file in file_handle:
-                    # do your thing
-                    yield single_file
-        """
-
         if self.debug:
-            print(filelist)
+            logger.info(filelist)
 
         return 0
