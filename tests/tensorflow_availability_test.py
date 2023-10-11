637dcf8625a7cf434cdf432f5b87e00f1c62f471
bkutasi <balazs@kutasi.dev>
2023-10-11 23:11:21 +0200

    Tensorflow tests improvement

--- tests/tensorflow_availability_test.py
+++ tests/tensorflow_availability_test.py
@@ -1,6 +1,7 @@
 import unittest
 import tensorflow as tf
 
+
 class TestAvailability(unittest.TestCase):
     def test_tensorflow_import(self):
         try:
@@ -11,7 +12,7 @@ class TestAvailability(unittest.TestCase):
 
     def test_gpu_availability(self):
         try:
-            gpus = tf.config.list_physical_devices('GPU')
+            gpus = tf.config.list_physical_devices("GPU")
             self.assertTrue(len(gpus) > 0, "No GPU available")
         except AttributeError:
             self.assertTrue(False, "TensorFlow is not available to check for GPUs")
