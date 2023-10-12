5c2edafa4286c1493585362738a934aa41f4e136
bkutasi <balazs@kutasi.dev>
2023-10-12 17:04:31 +0200

    Added logging class and testing

---
+++ tests/logging_test.py
@@ -0,0 +1,38 @@
+import unittest
+import os
+from utils.logging import Logger
+
+class TestLogger(unittest.TestCase):
+    def setUp(self):
+        self.logger = Logger('test_logger')
+        self.log_file = 'test_logger.log'
+
+    def tearDown(self):
+        if os.path.exists(self.log_file):
+            os.remove(self.log_file)
+
+    def test_debug(self):
+        self.logger.debug('This is a debug message')
+        self.assertLogContains('DEBUG', 'This is a debug message')
+
+    def test_info(self):
+        self.logger.info('This is an info message')
+        self.assertLogContains('INFO', 'This is an info message')
+
+    def test_warning(self):
+        self.logger.warning('This is a warning message')
+        self.assertLogContains('WARNING', 'This is a warning message')
+
+    def test_error(self):
+        self.logger.error('This is an error message')
+        self.assertLogContains('ERROR', 'This is an error message')
+
+    def test_critical(self):
+        self.logger.critical('This is a critical message')
+        self.assertLogContains('CRITICAL', 'This is a critical message')
+
+    def assertLogContains(self, level, message):
+        with open(self.log_file, 'r') as f:
+            log_contents = f.read()
+            self.assertIn(level, log_contents)
+            self.assertIn(message, log_contents)
---
+++ utils/logging.py
@@ -0,0 +1,35 @@
+import logging
+
+
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        file_handler = logging.FileHandler(f"{name}.log")
+        file_handler.setFormatter(formatter)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(stream_handler)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
