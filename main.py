9245b3188e52a7b11b28330b145a179c80cb290a
bkutasi <balazs@kutasi.dev>
2023-10-13 23:49:10 +0200

    Rewrite logic

--- main.py
+++ main.py
@@ -1,48 +1,42 @@
 import numpy as np
-import os
 import loader
 import nn
-import tensorflow as tf
+from utils.dataset_download_utils import Downloader
+from utils.dataset_utils import DatasetUtils
 
-# TODO make runs with non-smote + balanced data (reduce rest) + keep test balanced
-# TODO try to compare the data with oversampling the non-rest, try the same for test
+if __name__ == "__main__":
+    downloader = Downloader(
+        url="https://physionet.org/static/published-projects/eegmmidb/eeg-motor-movementimagery-dataset-1.0.0.zip"
+    )
 
-# List all available GPUs
-gpus = tf.config.list_physical_devices('GPU')
+    downloader.download()
 
-print("Available GPUs:")
-for gpu in gpus:
-    print(gpu.name)
+    dataset_utils = DatasetUtils()
+    dataset_utils.generate()
 
-# Only use the first available gpu/device
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    x, y = loader.FileLoader.load_saved_files()
+    # x_fft, y_fft = nn.NeuralNets.fft_processor(x_no_smote, y_no_smote)
+    # x_no_rest, y_no_rest = loader.FileLoader.data_equalizer(x_no_smote, y_no_smote)
+    print("Shape of x: ", np.shape(x))
 
-# generator.Generator.generate()
+    # print('Instances of classes no SMOTE: ', y_no_smote.sum(axis=1).sum(axis=0))
+    # print('Instances of classes data eq: ', y_no_rest.sum(axis=1).sum(axis=0))
+    print("Instances of classes after SMOTE: ", y.sum(axis=1).sum(axis=0))
 
-x, y = loader.FileLoader.load_saved_files()
-# x_fft, y_fft = nn.NeuralNets.fft_processor(x_no_smote, y_no_smote)
-# x_no_rest, y_no_rest = loader.FileLoader.data_equalizer(x_no_smote, y_no_smote)
-print("Shape of x: ", np.shape(x))
+    history, model, acc, avgAcc = nn.NeuralNets.loo(x, y)
 
-# print('Instances of classes no SMOTE: ', y_no_smote.sum(axis=1).sum(axis=0))
-# print('Instances of classes data eq: ', y_no_rest.sum(axis=1).sum(axis=0))
-print("Instances of classes after SMOTE: ", y.sum(axis=1).sum(axis=0))
+    # model = nn.NeuralNets.one_d_cnn_multi()
+    # nn.NeuralNets.generator_processor(model, x_no_smote, y_no_smote)
+    # models, history_loo, acc, avgAcc = nn.NeuralNets.leave_one_out(x_no_smote, y_no_smote)
 
-history, model, acc, avgAcc = nn.NeuralNets.loo(x, y)
+    with open("avg accuracy.txt", "a") as f:
+        f.write(str(avgAcc) + "\n")
 
-# model = nn.NeuralNets.one_d_cnn_multi()
-# nn.NeuralNets.generator_processor(model, x_no_smote, y_no_smote)
-# models, history_loo, acc, avgAcc = nn.NeuralNets.leave_one_out(x_no_smote, y_no_smote)
+    print("Final avg accuracy:", avgAcc)
 
-with open("avg accuracy.txt", "a") as f:
-    f.write(str(avgAcc) + "\n")
+    x, y, x_no_smote, y_no_smote = loader.FileLoader.load_saved_files_val()
+    nn.NeuralNets.metrics_generation(model, x_no_smote, y_no_smote, "1DCNN")
+    nn.NeuralNets.plot_roc_curve(model, x_no_smote, y_no_smote, "1DCNN")
 
-print("Final avg accuracy:", avgAcc)
-
-x, y, x_no_smote, y_no_smote = loader.FileLoader.load_saved_files_val()
-nn.NeuralNets.metrics_generation(model, x_no_smote, y_no_smote, "1DCNN")
-nn.NeuralNets.plot_roc_curve(model, x_no_smote, y_no_smote, "1DCNN")
-
-
-"""nn.NeuralNets.metrics_generation(models['FCN'], x_no_smote, y_no_smote)
-nn.NeuralNets.plot_roc_curve(models['FCN'], x_no_smote, y_no_smote, "FCN.png")"""
+    """nn.NeuralNets.metrics_generation(models['FCN'], x_no_smote, y_no_smote)
+    nn.NeuralNets.plot_roc_curve(models['FCN'], x_no_smote, y_no_smote, "FCN.png")"""
