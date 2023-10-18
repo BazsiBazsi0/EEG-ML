f8ea092707229597c9ac093919b6fb460ace55cb
bkutasi <balazs@kutasi.dev>
2023-10-18 20:47:30 +0200

    One-hot encoding rewrite and testing

--- tests/helpers/one_hot_test.py
+++ tests/helpers/one_hot_test.py
@@ -3,11 +3,11 @@ import numpy as np
 from utils.helpers.one_hot_helper import OneHotHelper
 from utils.logging_utils import Logger
 
-logger = Logger(__name__)
-
 class TestOneHotHelper(unittest.TestCase):
     def setUp(self):
         self.helper = OneHotHelper()
+        self.logger = Logger(__name__)
+
 
     def test_to_one_hot(self):
         # Test case 1: Normal case with multiple unique labels
@@ -15,25 +15,19 @@ class TestOneHotHelper(unittest.TestCase):
         expected = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]])
         result = self.helper.to_one_hot(y)
         np.testing.assert_array_equal(result, expected)
-        logger.debug("Test case 1 passed")
+        self.logger.debug("Test case 1 passed")
 
         # Test case 2: All elements are the same
         y = np.array([1, 1, 1, 1, 1])
         expected = np.array([[1], [1], [1], [1], [1]])
         result = self.helper.to_one_hot(y)
         np.testing.assert_array_equal(result, expected)
-        logger.debug("Test case 2 passed")
+        self.logger.debug("Test case 2 passed")
 
         # Test case 3: Normal case with non-sequential labels
         y = np.array([1, 3, 5, 3, 1])
         expected = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]])
         result = self.helper.to_one_hot(y)
         np.testing.assert_array_equal(result, expected)
-        logger.debug("Test case 3 passed")
+        self.logger.debug("Test case 3 passed")
 
-        # Test case 4: Normal case with negative labels
-        y = np.array([-1, -2, -3, -2, -1])
-        expected = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]])
-        result = self.helper.to_one_hot(y)
-        np.testing.assert_array_equal(result, expected)
-        logger.debug("Test case 4 passed")
--- utils/helpers/one_hot_helper.py
+++ utils/helpers/one_hot_helper.py
@@ -1,29 +1,33 @@
 import numpy as np
-from tensorflow.keras.utils import to_categorical
+from typing import Dict
 
 class OneHotHelper:
     @staticmethod
     def to_one_hot(y: np.ndarray) -> np.ndarray:
         """
-        Converts the input array to one-hot encoded array.
+        This static method transforms an input numpy array into a one-hot encoded array.
+        It identifies unique labels in the array and maps each unique label to a unique integer.
+        Then, it uses these mappings to convert the input array into a one-hot encoded array.
 
-        Parameters:
-            y (np.ndarray): The input array.
+        Args:
+            y (np.ndarray): The input array to be one-hot encoded.
 
         Returns:
-            np.ndarray: The one-hot encoded array.
+            np.ndarray: The one-hot encoded version of the input array.
         """
-        # Shallow copy to a new array
-        y_shallow_copy = y.copy()
-        
-        # New unique labels in case of double vals(maybe there are duplicates)
-        total_labels = np.unique(y_shallow_copy)
+        # Identify unique labels in the array
+        total_labels = np.unique(y)
 
-        # Dictionary named encoding for labels
-        encoding = {}
-        for x in range(len(total_labels)):
-            encoding[total_labels[x]] = x
-        for x in range(len(y_shallow_copy)):
-            y_shallow_copy[x] = encoding[y_shallow_copy[x]]
+        # Create a dictionary mapping each unique label to a unique integer
+        encoding: Dict[int, int] = {label: index for index, label in enumerate(total_labels)}
 
-        return to_categorical(y_shallow_copy)
+        # Create an array for one-hot encoding
+        one_hot_array = np.zeros((y.size, total_labels.size))
+
+        # Use the dictionary to convert the input array into a one-hot encoded array
+        for i in range(len(y)):
+            if y[i] not in encoding:
+                raise ValueError(f"Unexpected value {y[i]} at index {i}.")
+            one_hot_array[i, encoding[y[i]]] = 1
+
+        return one_hot_array
