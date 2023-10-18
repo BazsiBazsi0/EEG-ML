de0f900f1e0e53ed2660f906de2eb1b0c25f6e50
bkutasi <balazs@kutasi.dev>
2023-10-18 22:25:05 +0200

    Dataset loader refactoring

--- utils/dataset_loader_utils.py
+++ utils/dataset_loader_utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.preprocessing import minmax_scale
+from utils.helpers.one_hot_helper import OneHotHelper
+from utils.helpers.smote_helper import SmoteHelper
 
 class FileLoader:
     def __init__(self, patients=10, subjects=range(60, 70)):
@@ -8,7 +11,7 @@ class FileLoader:
     def load_saved_files_val(self):
         xs, ys = [], []
         for s in self.subjects:
-            # level 0 7, level 1
+            # levels loading
             xs.append(
                 np.load("dataset/filtered_data/ch_level_1/x_sub_{}.npy".format(s))
             )
@@ -46,3 +49,41 @@ class FileLoader:
         ).astype(np.float32)
 
         return x, y, x_no_smote, y_no_smote
+
+    def preprocessor(x: np.ndarray, y: np.ndarray):
+        """
+        Preprocesses the input data and returns the processed data.
+
+        Parameters:
+            x (np.ndarray): The input data.
+            y (np.ndarray): The labels of the input data.
+
+        Returns:
+            Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: The preprocessed data
+        """
+        # Reshaping to 2D
+        x_reshaped = np.reshape(x, (x.shape[0], x.shape[1] * x.shape[2])).astype(
+            np.float16
+        )
+
+        # Minmax scaling
+        x_scaled = minmax_scale(x_reshaped, axis=1).astype(np.float16)
+
+        # One_hot encoding
+        y_one_hot = OneHotHelper.to_one_hot(y).astype(np.uintc)
+
+        # Synthetic Minority Oversampling Technique
+        x_smote, y_smote = SmoteHelper.smote_processor(x_scaled, y_one_hot)
+
+        # Reshaping back into the orig 3D format
+        # Structure: Epochs x Channels x Datapoints
+        x_smote = np.reshape(
+            x_smote, (x_smote.shape[0], x.shape[1], x_smote.shape[1] // x.shape[1])
+        )
+
+        x_no_smote = np.reshape(
+            x_scaled, (x_scaled.shape[0], x.shape[1], x_scaled.shape[1] // x.shape[1])
+        )
+
+        return x_smote, y_smote, x_no_smote, y_one_hot
+
