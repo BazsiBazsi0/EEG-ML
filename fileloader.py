2576b4ba9fd2b3787f844774a80b6ca75fa2a142
bkutasi <balazs@kutasi.dev>
2024-01-16 20:05:12 +0100

    Refactor FileLoader class to load all patients and set dataset paths

--- fileloader.py
+++ fileloader.py
@@ -1,7 +1,10 @@
 import numpy as np
 from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import MinMaxScaler
 import config
 from fileprocessor import FileProcessor
+from typing import List
+import os
 
 
 class FileLoader:
@@ -12,28 +15,32 @@ class FileLoader:
         # for x_no smote with 1 patient its 1 x 160 x 64 x 641 ( one patient with 160 epoch, 64 chs, and 641 datapoints)
         # for y_no_smote with 1 patient its 1 x 160 x 5 (1 patient with 160 epoch and 5 classes in each epoch)
 
-        # load ten patients randomly from the saved files
-        # TODO: load random patients not preselected ones
-        patients = 10
-        subjects = range(1, 11)
-        for i in range(patients):
-            # Generate a random number between 0 and 103
-            random_number = np.random.randint(0, 103)
-            # Check if the random number is in the list of exceptions
-            if random_number in config.excluded:
-                # If it is, generate a new random number
-                random_number = np.random.randint(0, 103)
-                continue
-            # Add the random number to the list
-            # subjects.append(random_number)
+        # Set the paths for the dataset and filtered data
+        dataset_path: str = config.dataset_path
+        filtered_data_path: str = os.path.join(
+            dataset_path, "filtered_data", "ch_level_0"
+        )
+        # Get a list of all x files in the filtered data path
+        x_files: List[str] = [
+            file for file in os.listdir(filtered_data_path) if file.startswith("x_")
+        ]
+        # Determine the number of patients based on the number of x files
+        patients_full: int = len(x_files)
+        # Create a list of subjects from 1 to the number of patients
+        subjects: List[int] = list(range(1, patients_full + 1))
+        # Get a list of excluded subjects from the config file
+        excluded_subjects: List[int] = config.excluded
+        # Remove the excluded subjects from the list of subjects
+        subjects = set(set(subjects) - set(excluded_subjects))
+        patients: int = len(subjects)
 
         xs, ys = [], []
         for s in subjects:
             xs.append(
-                np.load("dataset/filtered_data/ch_level_2/x_sub_{}.npy".format(s))
+                np.load("dataset/filtered_data/ch_level_0/x_sub_{}.npy".format(s))
             )
             ys.append(
-                np.load("dataset/filtered_data/ch_level_2/y_sub_{}.npy".format(s))
+                np.load("dataset/filtered_data/ch_level_0/y_sub_{}.npy".format(s))
             )
 
         x_concat = np.concatenate(xs)
@@ -69,7 +76,7 @@ class FileLoader:
 
         x_no_smote = x_no_smote_pre.reshape(
             patients,
-            x_no_smote_pre.shape[0] // patients,
+            x_no_smote_pre.shape[0] // patients,  # 12320 / 91 = 135???
             x_no_smote_pre.shape[1],
             x_no_smote_pre.shape[2],
         ).astype(np.float32)
