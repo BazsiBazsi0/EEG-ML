1955d612d4b113f671859968665e80ea331f6be7
bkutasi <balazs@kutasi.dev>
2024-01-27 20:05:12 +0100

    Function calls correctly specified

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from utils.dataset_utils import DatasetUtils
 if __name__ == "__main__":
 
     # Only use the first available gpu/device
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
     # Initialize the download class
     downloader = Downloader(
@@ -28,17 +28,17 @@ if __name__ == "__main__":
     # Data loading
     # TODO: Break apart the loader to a processor and a loader
     x, y, x_no_smote, y_no_smote = fileloader.FileLoader.load_saved_files()
+    # TODO: Proper logging
     print("Shape of x: ", np.shape(x))
     print("Instances of classes before SMOTE: ", y_no_smote.sum(axis=1).sum(axis=0))
     print("Instances of classes after SMOTE: ", y.sum(axis=1).sum(axis=0))
 
-    # Execute leave one out validation with a predefined model
-    history, model, acc, avgAcc = nn.NeuralNets.loo(x, y)
+    # Data loading and preprocessing done, time to train the model
+    # Execute leave one out(k_fold n=10) validation with a predefined model
+    # TODO: Document kfold, Plateou LR reduction(0.0001 and try later 0.00001) and later OneCycleScheduler
+    history, model, acc, avgAcc = nn.NeuralNets.k_fold_validation(x, y)
 
-    # model = nn.NeuralNets.one_d_cnn_multi()
-    # nn.NeuralNets.generator_processor(model, x_no_smote, y_no_smote)
-    # models, history_loo, acc, avgAcc = nn.NeuralNets.leave_one_out(x_no_smote, y_no_smote)
-
+    # TODO: Proper logging
     # Accuracy saving
     with open("avg accuracy.txt", "a") as f:
         f.write(str(avgAcc) + "\n")
