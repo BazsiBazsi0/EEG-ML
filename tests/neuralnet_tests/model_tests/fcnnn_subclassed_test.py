125712952de8aa012acfd71bc58f32bddbdfae56
bkutasi <balazs@kutasi.dev>
2024-03-06 23:25:36 +0100

    Add test methods for call and fit in FCNNModel

--- tests/neuralnet_tests/model_tests/fcnnn_subclassed_test.py
+++ tests/neuralnet_tests/model_tests/fcnnn_subclassed_test.py
@@ -1,5 +1,6 @@
-import unittest
+import numpy as np
 import tensorflow as tf
+import unittest
 from neuralnets.models.fcnnmodel_subclassed import FCNNModel
 
 
@@ -7,18 +8,49 @@ class TestFCNNModel(unittest.TestCase):
     def setUp(self):
         self.load_level = 0
         self.electrodes = 7
+        self.model = FCNNModel.create_and_compile(self.load_level, self.electrodes)
+
+        # Create some random training data
+        self.x_train = np.random.rand(10, 641, self.electrodes)
+        self.y_train = np.random.randint(0, 5, 10)
+
+        # Convert labels to categorical
+        self.y_train = tf.keras.utils.to_categorical(self.y_train, 5)
 
     def test_create_and_compile(self):
-        model = FCNNModel.create_and_compile(self.load_level, self.electrodes)
-        self.assertIsInstance(model, tf.keras.Model)
-        self.assertEqual(model.name, f"FCNN_ch_level_{self.load_level}")
-        self.assertEqual(model.loss, tf.keras.losses.categorical_crossentropy)
+        self.assertIsInstance(self.model, tf.keras.Model)
+        self.assertEqual(self.model.name, f"FCNN_ch_level_{self.load_level}")
+        self.assertEqual(self.model.loss, tf.keras.losses.categorical_crossentropy)
 
         # Check the optimizer
-        self.assertIsInstance(model.optimizer, tf.keras.optimizers.Optimizer)
+        self.assertIsInstance(self.model.optimizer, tf.keras.optimizers.Optimizer)
 
         # Check the number of layers in the model
-        self.assertGreater(len(model.layers), 0)
+        self.assertGreater(len(self.model.layers), 0)
 
         # Check the type of the first layer
-        self.assertIsInstance(model.layers[0], tf.keras.layers.Layer)
+        self.assertIsInstance(self.model.layers[0], tf.keras.layers.Layer)
+
+    def test_call(self):
+        # Create a random tensor to pass to the call method
+        input_tensor = tf.random.uniform((1, 64, 64, 1))
+        output_tensor = self.model.call(input_tensor)
+
+        # Check that the output is a tensor
+        self.assertIsInstance(output_tensor, tf.Tensor)
+
+        # Check that the output shape is as expected
+        self.assertEqual(output_tensor.shape, (1, 5))
+
+    def test_fit(self):
+        # Fit the model, note batch size is 10 - the size of the training data
+        history = self.model.fit(
+            self.x_train, self.y_train, epochs=1, verbose=0, batch_size=10
+        )
+
+        # Check that history object is returned
+        self.assertIsInstance(history, tf.keras.callbacks.History)
+
+        # Check that history object has loss and accuracy
+        self.assertIn("loss", history.history)
+        self.assertIn("accuracy", history.history)
